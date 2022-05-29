c1546367790a8fb6d43c96e7d75d716efe0dabab
jacobh9 <81500683+jacobh9@users.noreply.github.com>
2022-05-29 11:25:13 +1000

    Got upload file working

--- src/Main/SimpleClient.java
+++ src/Main/SimpleClient.java
@@ -1,12 +1,56 @@
 package Main;
 import java.util.*;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
 import java.net.*;
 import java.io.*;
 
 public class SimpleClient {
+	
+	public static void uploadfile() {
+		try {
+	          String host = "131.217.172.91";
+	          String user = "ubuntu";
+	          String privateKey = "C:\\Users\\jhanc\\Downloads\\TestKeyPrivate.pem"; //please provide your ppk file
+	          JSch jsch = new JSch();
+	          Session session = jsch.getSession(user, host, 22);
+	          Properties config = new Properties();
+	         // session.setPassword("KIT418@utas"); ////if password is empty please comment it
+	          jsch.addIdentity(privateKey);
+	          System.out.println("identity added "); 
+	          config.put("StrictHostKeyChecking", "no");
+	          session.setConfig(config);
+	          session.connect();
+	       
+	          Channel channel = session.openChannel("sftp");
+	          channel.connect();
+	          ChannelSftp sftpChannel = (ChannelSftp) channel;
+	          sftpChannel.put("C:\\Users\\jhanc\\Downloads\\test\\test.txt", "/home/ubuntu/Input/test.text"); //Provide different input file/folder
+	         
+	          System.out.println("done");
+
+	          sftpChannel.exit();
+	          session.disconnect();
+	      } catch (JSchException e) {
+	          e.printStackTrace();
+	      } catch (SftpException e) {
+	          e.printStackTrace();
+	      }
+			catch(Exception e){
+	      	   System.out.println(e);
+			}   
+		}
+	
 	public static void main(String args[]) throws IOException {
 		//Open your connection to a server, at port 1254
-		Socket clientSocket = new Socket("127.0.0.1"/*"131.217.172.91"*/,1254); // The first address is to test code locally, the second connects to the nectar cloud.
+		//Socket clientSocket = new Socket("127.0.0.1"/*"131.217.172.91"*/,1254); // The first address is to test code locally, the second connects to the nectar cloud.
+		Socket clientSocket = new Socket("131.217.172.91",1254); 
 		Scanner input = new Scanner(System.in);
 		String sending = "";
 		
@@ -20,8 +64,6 @@ public class SimpleClient {
 		ct.start();
 		  
 		
-		
-		
 		// Until an error occurs, wait for the user to input text and then send that text to the server.
 		try
 		{
@@ -29,6 +71,11 @@ public class SimpleClient {
 			{
 				sending = new String(input.nextLine());
 				dos.writeUTF(sending);
+				
+				if (sending.equals("File"))
+				{
+					uploadfile();
+				}
 			}
 		}
 		catch(IOException e) {e.printStackTrace();}
